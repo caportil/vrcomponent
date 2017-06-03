ac1dc2b16f013cac2df16f67530fb005061dece0
caportil <caportil@gmail.com>
2017-06-03 01:57:31 -0700

    Implement custom 2D video within 3D videosphere

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom';
 import Camera from './Camera';
 import Sky from './Sky';
 import Videosphere from './Videosphere';
+import VideoPlane from './VideoPlane';
 
 class App extends Component {
   constructor(props) {
@@ -18,6 +19,11 @@ class App extends Component {
       <Scene>
         <Entity geometry={{primitive: 'box'}} material={{color: 'red'}} position={{x: 0, y: 0, z: -5}}/>
         <Videosphere src="https://ucarecdn.com/bcece0a8-86ce-460e-856b-40dac4875f15/"/>
+        <VideoPlane 
+          geometry={{height: 3, width: 3}}
+          position={{x:0, y:0, z:-3}}
+          src="https://ucarecdn.com/bcece0a8-86ce-460e-856b-40dac4875f15/" 
+        />
       </Scene>
 
     );
