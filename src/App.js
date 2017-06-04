aeea329919fc64045b96bb695fcb6bea0b3ca2f3
caportil <caportil@gmail.com>
2017-06-04 01:49:08 -0700

    Add raycaster interactivity with click/intersect functionality via events prop

--- src/App.js
+++ src/App.js
@@ -14,6 +14,19 @@ class App extends Component {
       //
     }
   }
+
+  handleClick() {
+    console.log('Sphere clicked!');
+  }
+
+  handleClick2() {
+    console.log('Box clicked!');
+  }
+
+  handleCollide(data) {
+    console.log('Collision at:', data)
+  }
+
   render() {
     {/*
     return (
@@ -32,16 +45,27 @@ class App extends Component {
 
     return (
       <Scene>
-      <Camera>
-        <a-cursor
-          animation__click="property: scale; easing: easeOutQuad; startEvents: click; from: 2 2 2; to: 1 1 1; dur: 200"
-          geometry="radiusInner:0.02; radiusOuter:0.03; segmentsTheta:64"
-          material={"color:#61ffff; shader: flat"}
-          >
-        </a-cursor>
-      </Camera>
-        <Entity primitive='a-sky' src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/city.jpg"/>
-        <iframe src="https://www.w3schools.com"></iframe>
+        <Camera>
+          <a-cursor
+            animation__click="property: scale; easing: easeOutQuad; startEvents: click; from: 2 2 2; to: 1 1 1; dur: 200"
+            geometry="radiusInner:0.02; radiusOuter:0.03; segmentsTheta:64"
+            material={"color:#61ffff; shader: flat"}
+            >
+          </a-cursor>
+        </Camera>
+
+        <Entity primitive='a-sky' src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/city.jpg" />
+
+        <Entity
+          rotation="0 180 0"
+          position="0 0 0"
+          material={{opacity: '0', shader: 'flat', side: 'double', transparent: 'true', repeat: '-1 1'}}
+          geometry="height:5;primitive:sphere;radius:7.5;segmentsRadial:48;thetaLength:360;openEnded:true;thetaStart:0"
+          events={{'click': this.handleClick}}
+        />
+
+        <Entity geometry={{primitive: 'box'}} material={{color: 'red'}} position={{x: 0, y: 0, z: -5}} events={{'raycaster-intersected': this.handleCollide, click: this.handleClick2}} />
+
       </Scene>
     )
   }
