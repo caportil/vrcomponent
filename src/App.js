be304f2e9fa4f0fed8998eb4260cb804732b2b79
caportil <caportil@gmail.com>
2017-06-04 00:26:20 -0700

    Implement camera + cursor components

--- src/App.js
+++ src/App.js
@@ -15,9 +15,11 @@ class App extends Component {
     }
   }
   render() {
+    {/*
     return (
       <Scene>
         <Entity geometry={{primitive: 'box'}} material={{color: 'red'}} position={{x: 0, y: 0, z: -5}}/>
+
         <Videosphere src="https://ucarecdn.com/bcece0a8-86ce-460e-856b-40dac4875f15/"/>
         <VideoPlane 
           geometry={{height: 3, width: 3}}
@@ -25,8 +27,23 @@ class App extends Component {
           src="https://ucarecdn.com/bcece0a8-86ce-460e-856b-40dac4875f15/" 
         />
       </Scene>
-
     );
+  */}
+
+    return (
+      <Scene>
+      <Camera>
+        <a-cursor
+          animation__click="property: scale; easing: easeOutQuad; startEvents: click; from: 2 2 2; to: 1 1 1; dur: 200"
+          geometry="radiusInner:0.02; radiusOuter:0.03; segmentsTheta:64"
+          material={"color:#61ffff; shader: flat"}
+          >
+        </a-cursor>
+      </Camera>
+        <Entity primitive='a-sky' src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/city.jpg"/>
+        <iframe src="https://www.w3schools.com"></iframe>
+      </Scene>
+    )
   }
 }
 
