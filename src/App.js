ebdb07b8220c3686f7e58519a1b8f0bb81433ff8
caportil <caportil@gmail.com>
2017-06-04 11:41:44 -0700

    Add toggle for hiddensphere/collision functionality

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      //
+      toggle: false
     }
   }
 
@@ -20,7 +20,8 @@ class App extends Component {
   }
 
   handleClick2() {
-    console.log('Box clicked!');
+    this.setState({toggle: !this.state.toggle})
+    console.log('Box clicked! Current toggle state:', this.state.toggle);
   }
 
   handleCollide(data) {
@@ -56,17 +57,26 @@ class App extends Component {
           </a-cursor>
         </Camera>
 
-        <Videosphere src="https://ucarecdn.com/bcece0a8-86ce-460e-856b-40dac4875f15/"/>
+        <Videosphere id='videosphere' src="https://ucarecdn.com/bcece0a8-86ce-460e-856b-40dac4875f15/"/>
 
-        <Entity
-          rotation="0 180 0"
-          position="0 0 0"
-          material={{opacity: '0', shader: 'flat', side: 'double', transparent: 'true', repeat: '-1 1'}}
-          geometry="height:5;primitive:sphere;radius:7.5;segmentsRadial:48;thetaLength:360;openEnded:true;thetaStart:0"
-          events={{'click': this.handleClick}}
-        />
 
-        <Entity geometry={{primitive: 'box'}} material={{color: 'red'}} position={{x: 0, y: 0, z: -5}} events={{'raycaster-intersected': this.handleCollide, click: this.handleClick2}} />
+        {this.state.toggle? 
+
+          <Entity
+            id='hiddenSphere'
+            rotation="0 180 0"
+            position="0 0 0"
+            material={{opacity: '0', shader: 'flat', side: 'double', transparent: 'true', repeat: '-1 1'}}
+            geometry="height:5;primitive:sphere;radius:7.5;segmentsRadial:48;thetaLength:360;openEnded:true;thetaStart:0"
+            events={{'raycaster-intersected': this.handleCollide, 'click': this.handleClick}}
+          />
+
+          :
+
+          null
+        }
+
+        <Entity geometry={{primitive: 'box'}} material={{color: 'red'}} position={{x: 0, y: 0, z: -5}} events={{click: this.handleClick2.bind(this)}} />
 
       </Scene>
     )
