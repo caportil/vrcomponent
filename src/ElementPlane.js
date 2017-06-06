20d47439555c89f6f70cc6678159946e291a3d11
caportil <caportil@gmail.com>
2017-06-05 22:48:46 -0700

    Add text elements + text bar placeholder with instructions

--- src/ElementPlane.js
+++ src/ElementPlane.js
@@ -6,7 +6,7 @@ export default props => (
 
     {/* Background Plane */}
     <Entity
-      geometry={{primitive: 'plane', height: 5, width: 4}}
+      geometry={{primitive: 'plane', height: 7, width: 4}}
       material={{color: 'black'}}
     />
 
@@ -17,7 +17,7 @@ export default props => (
         color: 'white',
         width: 8
       }}
-      position={{x: 2.5, y: 1.70, z: 0.25}}
+      position={{x: 2.5, y: 2.70, z: 0.25}}
       rotation='0 0 0'
     />
 
@@ -25,7 +25,7 @@ export default props => (
     <Entity
       geometry={{primitive: 'plane'}}
       material={{src: 'https://d30y9cdsu7xlg0.cloudfront.net/png/40974-200.png', shader: 'flat', side: 'double', opacity: 1}}
-      position={{x: -1.2, y: 0.75, z: 0.05}}
+      position={{x: -1.2, y: 1.75, z: 0.05}}
       rotation='0 0 0'
     />
 
@@ -33,7 +33,7 @@ export default props => (
     <Entity
       geometry={{primitive: 'plane'}}
       material={{src: 'https://d30y9cdsu7xlg0.cloudfront.net/png/40974-200.png', shader: 'flat', side: 'double', opacity: 1}}
-      position={{x: 0, y: 0.75, z: 0.05}}
+      position={{x: 0, y: 1.75, z: 0.05}}
       rotation='0 0 0'
     />
 
@@ -41,7 +41,37 @@ export default props => (
     <Entity
       geometry={{primitive: 'plane'}}
       material={{src: 'https://d30y9cdsu7xlg0.cloudfront.net/png/40974-200.png', shader: 'flat', side: 'double', opacity: 1}}
-      position={{x: 1.2, y: 0.75, z: 0.05}}
+      position={{x: 1.2, y: 1.75, z: 0.05}}
+      rotation='0 0 0'
+    />
+
+    {/* "Enter URL/text" element */}
+    <Entity
+      text={{
+        value: 'click to enter text',
+        color: 'white',
+        width: 8
+      }}
+      position={{x: 2.5, y: 0.4, z: 0.05}}
+      rotation='0 0 0'
+    />
+
+    {/* "Clickable Address Bar Temp */}
+    <Entity
+      geometry={{primitive: 'plane', height: 0.5, width: 3}}
+      material={{color: 'blue'}}
+      position={{x: 0, y: -0.25, z: 0.05}}
+      rotation='0 0 0'
+    />
+
+    {/* Instructions for Alert Pop Ups */}
+    <Entity
+      text={{
+        value: '1) enter url/text (make sure link is valid!)\n2) enter desired height + width + opacity',
+        color: 'white',
+        width: 4
+      }}
+      position={{x: 0.4, y: -1, z: 0.05}}
       rotation='0 0 0'
     />
 
