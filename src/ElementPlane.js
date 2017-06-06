d10c14d4b757a654438ca9e22a9f6e1983e0474c
caportil <caportil@gmail.com>
2017-06-05 23:34:44 -0700

    Implement view toggling within ElementPlane based on user selections

--- src/ElementPlane.js
+++ src/ElementPlane.js
@@ -9,6 +9,8 @@ export default props => {
   return (
     <Entity position={props.position} rotation={props.rotation}>
 
+    {/* STEP ONE (Displayed from beginning) */}
+
       {/* Background Plane */}
       <Entity
         geometry={{primitive: 'plane', height: 7, width: 4}}
@@ -50,55 +52,86 @@ export default props => {
         rotation='0 0 0'
       />
 
-      {/* "Enter URL/text" element */}
-      <Entity
-        text={{
-          value: 'click to enter text',
-          color: 'white',
-          width: 8
-        }}
-        position={{x: 2.6, y: 0.4, z: 0.05}}
-        rotation='0 0 0'
-      />
 
-      {/* "Clickable Address Bar Temp */}
-      <Entity
-        geometry={{primitive: 'plane', height: 0.5, width: 3}}
-        material={{color: 'blue'}}
-        position={{x: 0, y: -0.25, z: 0.05}}
-        rotation='0 0 0'
-      />
+    {/* STEP TWO (displayed after user selects element type) */}
 
-      {/* Instructions for Alert Pop Ups */}
-      <Entity
-        text={{
-          value: '1) enter url/text (make sure link is valid!)\n2) enter desired height + width + opacity',
-          color: 'white',
-          width: 4
-        }}
-        position={{x: 0.4, y: -1, z: 0.05}}
-        rotation='0 0 0'
-      />
+      { props.selections.type ?
+          
+        <Entity>
 
-    {/* "Preview" element */}
-      <Entity
-        text={{
-          value: 'preview',
-          color: 'white',
-          width: 8
-        }}
-        position={{x: 3.325, y: -1.75, z: 0.05}}
-        rotation='0 0 0'
-      />
+          {/* "Enter URL/text" element */}
+          <Entity
+            text={{
+              value: 'click to enter text',
+              color: 'white',
+              width: 8
+            }}
+            position={{x: 2.6, y: 0.4, z: 0.05}}
+            rotation='0 0 0'
+          />
 
-      {/* "Clickable Address Bar Temp */}
-      <Entity
-        geometry={{primitive: 'plane', height: 0.75, width: 1.5}}
-        material={{color: 'blue'}}
-        position={{x: -0.05, y: -2.5, z: 0.05}}
-        rotation='0 0 0'
-        events={{click: logSelections}}
-      />
+          {/* "Clickable Address Bar Temp */}
+          <Entity
+            geometry={{primitive: 'plane', height: 0.5, width: 3}}
+            material={{color: 'blue'}}
+            position={{x: 0, y: -0.25, z: 0.05}}
+            rotation='0 0 0'
+          />
+
+          {/* Instructions for Alert Pop Ups */}
+          <Entity
+            text={{
+              value: '1) enter url/text (make sure link is valid!)\n2) enter desired height + width + opacity',
+              color: 'white',
+              width: 4
+            }}
+            position={{x: 0.4, y: -1, z: 0.05}}
+            rotation='0 0 0'
+          />
+
+        </Entity>
+
+        :
+
+        null
+
+      }
+
+    {/* STEP THREE (displayed when element selections are complete) */}
+
+      { props.selections.previewable ?
+
+        <Entity>
+        
+          {/* "Preview" element */}
+          <Entity
+            text={{
+              value: 'preview',
+              color: 'white',
+              width: 8
+            }}
+            position={{x: 3.325, y: -1.75, z: 0.05}}
+            rotation='0 0 0'
+          />
+
+          {/* "Clickable Address Bar Temp */}
+          <Entity
+            geometry={{primitive: 'plane', height: 0.75, width: 1.5}}
+            material={{color: 'blue'}}
+            position={{x: -0.05, y: -2.5, z: 0.05}}
+            rotation='0 0 0'
+            events={{click: logSelections}}
+          />
+
+        </Entity>
+
+        :
+
+        null
+
+      }
+
+
 
     </Entity>
   )
