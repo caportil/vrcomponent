f56ccc835d77768be9f8a61348fbed77c8333a5c
caportil <caportil@gmail.com>
2017-06-06 21:27:53 -0700

    Fix bug where camera snaps back to 0-0-0 position on events

--- src/Camera.js
+++ src/Camera.js
@@ -2,7 +2,7 @@ import {Entity} from 'aframe-react';
 import React from 'react';
 
 export default props => (
-  <Entity>
-    <Entity camera="" position="0 0 0" rotation="15 0 0" look-controls="" wasd-controls="" {...props}/>
+  <Entity position="0 0 0" rotation="15 0 0">
+    <Entity camera="" look-controls="" wasd-controls="" {...props}/>
   </Entity>
 );
