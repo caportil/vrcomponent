ab43964d4bd698da434811ff0b606f64cd2d171f
caportil <caportil@gmail.com>
2017-06-17 16:48:34 -0700

    Integrate full 2D video render functionality in main component

--- src/VideoPlane.js
+++ src/VideoPlane.js
@@ -1,6 +1,13 @@
 import {Entity} from 'aframe-react';
 import React from 'react';
+import 'aframe-look-at-component';
 
 export default props => (
-  <Entity geometry={props.geometry} primitive="a-video" src={props.src} position={props.position}/>
+  <Entity
+    look-at='[camera]'
+    primitve='a-video'
+    geometry={{height: props.selections.styling.size, width: props.selections.styling.size, }} 
+    material={{src: props.selections.url, shader: 'flat', opacity: props.selections.styling.opacity}}
+    position={props.coordinates}
+  />
 );
