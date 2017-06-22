e2e4fcf8553adb6b3b8bbba99493c2cf75e6d949
caportil <caportil@gmail.com>
2017-06-21 23:08:25 -0700

    Update VideoPlane component to render flat plane instead of slightly non-flat box

--- src/VideoPlane.js
+++ src/VideoPlane.js
@@ -6,7 +6,7 @@ export default props => (
   <Entity
     look-at='[camera]'
     primitve='a-video'
-    geometry={{height: props.selections.styling.size, width: props.selections.styling.size, }} 
+    geometry={{primitive: 'plane', height: props.selections.styling.size, width: props.selections.styling.size, }} 
     material={{src: props.selections.url, shader: 'flat', opacity: props.selections.styling.opacity}}
     position={props.coordinates}
   />
